be74180c1240e187111ec8877f674f6ed167ecaf
Sean Dougherty <seanssavycoders@gmail.com>
2024-02-13 17:38:01 -0600

    Section03

---
+++ Section03/3.2/3.2.1-Arrays.js
@@ -0,0 +1,11 @@
+// create an Array using an Array literal
+let pizzaToppings = ["pineapple", "sausage", "jalapenos", "banana peppers"];
+// access the 1st item in the Array
+pizzaToppings[0];
+// access the last item in the Array
+pizzaToppings[3];
+// print the length of the Array
+pizzaToppings.length;
+// use the length property to access the last item in the Array
+pizzaToppings[pizzaToppings.length - 1];
+// with for...of, loop over the Array, modify the value and add to a different Array
